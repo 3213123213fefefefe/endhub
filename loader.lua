f7a723df9d58b642c10ff8f10681a91deb1c36a0
3213123213fefefefe <60438654+3213123213fefefefe@users.noreply.github.com>
2026-09-07 04:42:50 -0300

    Bust cache when loading EndHub

--- loader.lua
+++ loader.lua
@@ -1,6 +1,9 @@
 -- EndHub loader
-local url = "https://raw.githubusercontent.com/3213123213fefefefe/endhub/main/EndHub.lua"
+local nonce = tostring(os.time()) .. "-" .. tostring(math.random(100000, 999999))
+local url = "https://raw.githubusercontent.com/3213123213fefefefe/endhub/main/EndHub.lua?v=" .. nonce
 local source = game:HttpGet(url)
 local fn, err = loadstring(source)
-if not fn then error("[EndHub Loader] compile error: " .. tostring(err)) end
+if not fn then
+    error("[EndHub Loader] compile error: " .. tostring(err))
+end
 return fn()
